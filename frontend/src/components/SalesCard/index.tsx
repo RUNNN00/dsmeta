4e040b5237b516bb23387d132350f1071485b432
ruanazevedo <ruann.azevedo@outlook.com>
2022-07-29 20:37:26 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,8 +19,6 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
 
-        console.log(dmin);
-
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
             setSales(response.data.content);
         });
@@ -71,7 +69,7 @@ function SalesCard() {
                                         <td className="show992">{sale.visited}</td>
                                         <td className="show992">{sale.deals}</td>
                                         <td>R$ {sale.amount.toFixed(2)}</td>
-                                        <td><div className="dsmeta-button-container"><NotificationButton /></div></td>
+                                        <td><div className="dsmeta-button-container"><NotificationButton saleId={sale.id}/></div></td>
                                     </tr>
                                 )
                             })
